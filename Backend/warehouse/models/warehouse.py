9fc44673a751209987d1c5b7faea63d4353bfa7b
dunt <ntdu198@gmail.com>
2021-05-05 14:57:38 +0700

    create Inventory DB

--- Backend/warehouse/models/warehouse.py
+++ Backend/warehouse/models/warehouse.py
@@ -18,3 +18,18 @@ class Material(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Inventory(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    amount = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=18, decimal_places=0)
+    total_money = models.DecimalField(default=0, max_digits=18, decimal_places=0)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=tz.now)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Inventory_created_by")
+    last_updated_date = models.DateTimeField(null=True, blank=True)
+    last_updated_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name="Inventory_last_updated_by")
+
+    def __str__(self):
+        return '{0} {1}'.format(self.material, self.created_date.strftime('%d/%m/%Y'))
